e30a090194a592cae58d2ffb5489c5a6210e1b3e
Harshit Gindra <9598218+harshitgindra@users.noreply.github.com>
2025-03-08 15:48:29 -0800

    https://leetcode.com/problems/contains-duplicate-ii/

---
+++ LeetCodeSolutions/HashMap/Contains Duplicate II.cs
@@ -0,0 +1,56 @@
+﻿
+using NUnit.Framework;
+
+namespace LeetCodeSolutions.HashMap
+{
+    class Contains_Duplicate_II
+    {
+        public bool ContainsNearbyDuplicate(int[] nums, int k)
+        {
+            Dictionary<int, int> map = new Dictionary<int, int>();
+            for (int i = 0; i < nums.Length; i++)
+            {
+                if (map.ContainsKey(nums[i]))
+                {
+                    if (i - map[nums[i]] <= k)
+                    {
+                        return true;
+                    }
+                    else
+                    {
+                        map[nums[i]] = i;
+                    }
+                }
+                else
+                {
+                    map.Add(nums[i], i);
+                }
+            }
+
+            return false;
+        }
+
+        [Test(Description = "https://leetcode.com/problems/contains-duplicate-ii/")]
+        [Category("Easy")]
+        [Category("LeetCode")]
+        [Category("LeetCode 219")]
+        [Category("Contains Duplicate II")]
+        [TestCaseSource(nameof(Input))]
+        [Category("HashMap")]
+        [Category("TopInterview")]
+        public void Test1((bool Output, (int[], int) Input) item)
+        {
+            var response = ContainsNearbyDuplicate(item.Input.Item1, item.Input.Item2);
+            Assert.That(response, Is.EqualTo(item.Output));
+        }
+
+        public static IEnumerable<(bool Output, (int[], int) Input)> Input =>
+            new List<(bool Output, (int[], int) Input)>()
+            {
+                (true, ([1, 2, 3, 1], 3)),
+                (true, ([1, 0, 1, 1], 1)),
+                (false, ([1, 2, 3, 1, 2, 3], 2)),
+                (true, ([99,99], 1)),
+            };
+    }
+}
