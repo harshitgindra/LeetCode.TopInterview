870611c5557353cb2bae913fe53826c38af3d1c0
Harshit Gindra <9598218+harshitgindra@users.noreply.github.com>
2025-03-08 15:50:24 -0800

    https://leetcode.com/problems/valid-parentheses

---
+++ LeetCodeSolutions/Stack/Valid Parentheses.cs
@@ -0,0 +1,65 @@
+using NUnit.Framework;
+using NUnit.Framework.Legacy;
+
+namespace LeetCodeSolutions.Stack;
+
+public class Valid_Parentheses
+{
+    private Dictionary<char, char> _dictionary;
+    public bool IsValid(string s) {
+        _dictionary = new Dictionary<char, char>();
+        _dictionary.Add('(', ')');
+        _dictionary.Add('[', ']');
+        _dictionary.Add('{', '}');
+
+        Stack<char> stack = new Stack<char>();
+        var response = Helper(0, s, stack);
+        return response && stack.Count == 0;
+    }
+
+    private bool Helper(int i, string s, Stack<char> stack)
+    {
+        if (i < s.Length)
+        {
+            // If stack count is empty, it means we have found pairs for all parantheses so far
+            if (stack.Count == 0 || _dictionary[stack.Peek()] != s[i])
+            {
+                // Check if the char is opening parantheses
+                // if it's a closing parantheses, it should have found a match already
+                if (_dictionary.ContainsKey(s[i]))
+                {
+                    stack.Push(s[i]);
+                    return Helper(i + 1, s, stack);
+                }
+                return false;
+            }
+            else
+            {
+                stack.Pop();
+                return Helper(i + 1, s, stack);
+            }
+        }
+        return true;
+    }
+    
+    [Test(Description = "https://leetcode.com/problems/valid-parentheses/")]
+    [Category("Easy")]
+    [Category("LeetCode")]
+    [Category("Valid Parentheses")]
+    [TestCaseSource(nameof(Input))]
+    [Category("Stack")]
+    [Category("TopInterview")]
+    public void Test1((bool Output, string Input) item)
+    {
+        var response = IsValid(item.Input);
+        Assert.That(response, Is.EqualTo(item.Output));
+    }
+
+    public static IEnumerable<(bool Output, string Input)> Input =>
+        new List<(bool Output, string Input)>()
+        {
+            (true, "([])"),
+            (false, "(]"),
+            (false, "["),
+        };
+}
