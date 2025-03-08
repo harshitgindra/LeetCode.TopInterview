ec7f72d098a0e8fc808df1a558397b60682c4d8e
Harshit Gindra <9598218+harshitgindra@users.noreply.github.com>
2025-03-08 15:20:41 -0800

    https://leetcode.com/problems/remove-element/

---
+++ LeetCodeSolutions/ArrayAndString/RemoveElement.cs
@@ -0,0 +1,42 @@
+using NUnit.Framework;
+
+namespace LeetCodeSolutions.ArrayAndString;
+
+public class RemoveElementSolution
+{
+    public int RemoveElement(int[] nums, int val)
+    {
+        int j = 0;
+
+        for (int i = 0; i < nums.Length; i++)
+        {
+            if (nums[i] != val)
+            {
+                nums[j] = nums[i];
+                j++;
+            }
+        }
+
+        return j;
+    }
+
+    [Test(Description = "https://leetcode.com/problems/remove-element/")]
+    [Category("Easy")]
+    [Category("LeetCode")]
+    [Category("Array/String")]
+    [Category("TopInterview")]
+    [Category("Remove Element")]
+    [TestCaseSource(nameof(Input))]
+    public void Test1((int Output, (int[], int) Input) item)
+    {
+        var response = RemoveElement(item.Input.Item1, item.Input.Item2);
+        Assert.That(response, Is.EqualTo(item.Output));
+    }
+
+    public static IEnumerable<(int Output, (int[], int) Input)> Input =>
+        new List<(int Output, (int[], int) Input)>()
+        {
+            (2, ([3, 2, 2, 3], 3)),
+            (5, ([0, 1, 2, 2, 3, 0, 4, 2], 2)),
+        };
+}
