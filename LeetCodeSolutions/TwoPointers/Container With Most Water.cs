1351bdcc572cdf4dae105fa87f786670754f186a
Harshit Gindra <9598218+harshitgindra@users.noreply.github.com>
2025-04-15 23:26:20 -0700

    https://leetcode.com/problems/container-with-most-water/

---
+++ LeetCodeSolutions/TwoPointers/Container With Most Water.cs
@@ -0,0 +1,46 @@
+﻿namespace LeetCodeSolutions.TwoPointers
+{
+    public class ContainerWithMostWater
+    {
+        public int MaxArea(int[] height)
+        {
+            int returnValue = 0;
+
+            int left = 0;
+            int right = height.Length - 1;
+            while (left < right)
+            {
+                int distance = right - left;
+                int minHeight = Math.Min(height[left], height[right]);
+                int currentProduct = distance * minHeight;
+                returnValue = Math.Max(returnValue, currentProduct);
+
+                if (height[left] < height[right])
+                    left++;
+                else
+                    right--;
+            }
+
+            return returnValue;
+        }
+
+        [Test(Description = "https://leetcode.com/problems/container-with-most-water/")]
+        [Category("Medium")]
+        [Category("LeetCode")]
+        [Category("Container With Most Water")]
+        [TestCaseSource(nameof(Input))]
+        [Category("TwoPointers")]
+        [Category("TopInterview")]
+        public void Test1((int Output, int[] Input) item)
+        {
+            var response = MaxArea(item.Input);
+            Assert.That(response, Is.EqualTo(item.Output));
+        }
+
+        public static IEnumerable<(int Output, int[] Input)> Input =>
+            new List<(int Output, int[] Input)>()
+            {
+                (49, [1, 8, 6, 2, 5, 4, 8, 3, 7])
+            };
+    }
+}
--- README.md
+++ README.md
@@ -72,7 +72,7 @@ The Top Interview 150 collection on Leetcode is a curated set of 150 interview q
 | 25                                           |  Valid Palindrome                                        | Easy       |    ✅    |
 | 26                                           |  Is Subsequence                                          | Easy       |   ✅     |
 | 27                                           |  Two Sum II - Input Array Is Sorted                      | Medium     |   ✅     |
-| 28                                           |  Container With Most Water                               | Medium     |        |
+| 28                                           |  Container With Most Water                               | Medium     |   ✅     |
 | 29                                           |  3Sum                                                    | Medium     |        |
 | <br>        Sliding Window<br>               |                                                                                                           |            |        |
 | 30                                           |  Minimum Size Subarray Sum                               | Medium     |        |
